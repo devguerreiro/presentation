df8c5d477de0b2f256191542d5ced5b60d0038d2
Luis <luiscvlh11@gmail.com>
2022-02-09 02:03:32 +0000

    Finalizado v1

--- src/slideshows/presentation/Presentation.vue
+++ src/slideshows/presentation/Presentation.vue
@@ -49,11 +49,11 @@
       h1 Testes Manuais vs Testes Automáticos
 
     slide(enter='fadeIn')
-      h1 Mas qual ferramenta utilizar?
+      h1 E qual ferramenta utilizar?
 
     slide.comparacao(enter='fadeIn')
-      <img id="jest" src="@/jest.png" alt="Y2K-2000" height="450" width="450">
-      <img id="mocha" src="@/mocha.png" alt="Y2K-2000" height="450" width="450">
+      <img id="jest" src="@/jest.png" alt="Jest Logo" height="450" width="450">
+      <img id="mocha" src="@/mocha.png" alt="Mocha Logo" height="450" width="450">
 
     slide(:steps=5 enter='fadeIn')
       h1 Critérios de Avaliação
@@ -63,18 +63,20 @@
           li(v-if='step >= 3') Sintaxe ou Legibilidade
           li(v-if='step >= 4') Desempenho
           li(v-if='step >= 5') Curva de Aprendizado
-    slide(:steps=13 enter="fadeIn")
+    slide(:steps=3 enter='fadeIn')
       h1 Configuração Inicial
-      h2(v-if='step >= 1 && step <= 3') Instalação
+      h2 Instalação
       .instalacao
         eg-transition(enter='fadeIn')
-          eg-code-block(lang='bash' v-if='step >= 2 && step < 4') $ yarn add --dev jest
+          eg-code-block(lang='bash' v-if='step >= 2') $ yarn add --dev jest
         eg-transition(enter='fadeIn' )
-          eg-code-block(lang='bash' v-if='step >= 3 && step < 4') $ yarn add --dev mocha
-      h2(v-if='step >= 4') Escrita dos Testes
+          eg-code-block(lang='bash' v-if='step >= 3') $ yarn add --dev mocha
+    slide(enter='fadeIn')
+      h1 Configuração Inicial
+      h2 Escrita dos Testes
       .classe
         eg-transition(enter='fadeIn')
-          eg-code-block(v-if='step === 4' lang='javascript').
+          eg-code-block(lang='javascript').
             class Pessoa {
               MAIORIDADE = 18
 
@@ -89,19 +91,147 @@
             }
 
             module.exports = Pessoa;
+    slide(:steps=6 enter='fadeIn')
+      h1 Configuração Inicial
+      h2 Escrita dos Testes
+      .teste-mocha
         eg-transition(enter='fadeIn')
-          eg-code-block(v-if='step >= 5' lang='javascript').
-            const assert = require("assert")
-            const Pessoa = require("../Pessoa")
+          img(src="@/mocha.png" alt="Mocha Logo" height="150" width="150")
+        eg-transition(enter='fadeIn')
+          eg-code-block(lang='javascript').
+            const assert = require("assert") <eg-code-comment :active='step === 2' enter='flipInY'> Importa asserção do Node.js</eg-code-comment>
+            const Pessoa = require("../Pessoa") <eg-code-comment :active='step === 3' enter='flipInY'> Importa a classe Pessoa</eg-code-comment>
+
+            describe("Classe Pessoa", () => { <eg-code-comment :active='step === 4' enter='flipInY'> Define o escopo do teste</eg-code-comment>
+              it('deve retornar verdadeiro se uma pessoa for maior de idade', () => { <eg-code-comment :active='step === 5' enter='flipInY'> Descreve o teste</eg-code-comment>
+                const Fulano = new Pessoa("Fulano", 19)
+
+                assert.equal(Fulano.ehAdulta(), true) <eg-code-comment :active='step === 6' enter='flipInY'> Valida o retorno</eg-code-comment>
+              });
+            }
+    slide(:steps=3 enter='fadeIn')
+      h1 Configuração Inicial
+      h2 Escrita dos Testes
+      .teste-jest
+        eg-transition(enter='fadeIn')
+          img(src="@/jest.png" alt="Jest Logo" height="150" width="150")
+        eg-transition(enter='fadeIn')
+          eg-code-block(lang='javascript').
+            const Pessoa = require("./Pessoa") <eg-code-comment :active='step === 2' enter='flipInY'> Importa somente a classe</eg-code-comment>
 
             describe("Classe Pessoa", () => {
               it('deve retornar verdadeiro se uma pessoa for maior de idade', () => {
                 const Fulano = new Pessoa("Fulano", 19)
 
-                assert.equal(Fulano.ehAdulta(), true)
+                expect(Fulano.ehAdulta()).toBe(true) <eg-code-comment :active='step === 3' enter='flipInY'> Asserção integrada</eg-code-comment>
               });
             }
+    slide(enter='fadeIn')
+      h1 Configuração Inicial
+      h2 Reconhecimento dos testes
+      .dirs
+        #jest
+          eg-transition(enter='fadeIn')
+            img(src="@/dir-jest.png" alt="Jest Logo" height="400" width="700")
+          p Jest
+        #mocha
+          eg-transition(enter='fadeIn')
+            img(src="@/dir-mocha.png" alt="Jest Logo" height="400" width="700")
+          p Mocha
+    slide(:steps='3' enter='fadeIn')
+      h1 Sintaxe ou Legibilidade
+      .sintaxe
+        #mocha
+          img(src="@/mocha.png" alt="Mocha Logo" height="150" width="150")
+          eg-transition(enter='fadeIn')
+            eg-code-block(lang='javascript').
+              it('deve retornar verdadeiro se uma pessoa for maior de idade', () => { <eg-code-comment :active='step === 5' enter='flipInY'> Descreve o teste</eg-code-comment>
+                const Fulano = new Pessoa("Fulano", 19)
 
+                assert.equal(Fulano.ehAdulta(), true) <eg-code-comment :active='step === 2' enter='flipInY'> Leitura mais técnica</eg-code-comment>
+              });
+        #jest
+          img(src="@/jest.png" alt="Jest Logo" height="150" width="150")
+          eg-transition(enter='fadeIn')
+            eg-code-block(lang='javascript').
+              it('deve retornar verdadeiro se uma pessoa for maior de idade', () => {
+                const Fulano = new Pessoa("Fulano", 19)
+
+                expect(Fulano.ehAdulta()).toBe(true) <eg-code-comment :active='step === 3' enter='flipInY'> Leitura mais natural</eg-code-comment>
+              });
+    slide(:steps='6' enter='fadeIn')
+      h1 Desempenho
+      .desempenho
+        ul
+          li(v-if='step >= 2 && step < 5') Paralelização
+          li(v-if='step >= 3 && step < 5') Round Robin
+          li(v-if='step >= 4 && step < 5') Cache
+        table(v-if='step >= 5')
+          thead
+            tr
+              th Execução
+              th Resultado Mocha (s)
+              th Resulta Jest (s)
+          tbody
+            tr
+              td 1
+              td 0.3
+              td 0.194
+            tr
+              td 2
+              td 0.2
+              td 0.178
+            tr
+              td 3
+              td 0.3
+              td 0.221
+            tr
+              td 4
+              td 0.3
+              td 0.167
+            tr
+              td 5
+              td 0.3
+              td 0.177
+            tr(v-if='step === 6').bold
+              td Média
+              td 0.28
+              td 0.187
+    slide
+      h1 Curva de Aprendizado
+      .curva
+        img(src='@/graph.png' alt="Gráfico Pesquisa" height='450')
+        p Satisfação: usariam novamente / (usariam novamente + não usariam novamente)
+    slide
+      h1 Conclusão
+      .conclusao
+        table
+          thead
+            tr
+              th Critério
+              th Mocha
+              th Jest
+          tbody
+            tr
+              td Configuração Inicial
+              td A configuração inicial é simples em aplicações pouco complexas. Em aplicações mais complexas é necessário adicionar funcionalidades através de outras bibliotecas
+              td A configuração inicial é simples mesmo em aplicações mais complexas
+            tr
+              td Sintaxe/Legibilidade
+              td Dependendo da biblioteca utilizada em conjunto, a sintaxe/legibilidade pode não ser muito compreensível
+              td As APIs integradas são de fácil compreensão pois permitem uma leitura mais natural
+            tr
+              td Desempenho
+              td Possui desempenho inferior por executar por padrão em modo serial
+              td Possui melhor desempenho devido à paralelização, por utilizar round robin e fazer uso de cache com babel
+            tr
+              td Aprendizado
+              td Por possuir diversas bibliotecas compatíveis, é possível que se trabalhe em uma aplicação que utilize uma biblioteca da qual não possui conhecimento, sendo necessário aprender a utilizá-la
+              td Tem a maior parte da comunidade utilizando e satisfeitos. Quem domina Jest consegue trabalhar em qualquer aplicação que utilize Jest
+    slide
+      h1 Dúvidas?
+      .duvidas
+        img(src='@/confused.gif' width='700px')
 </template>
 
 <script>
@@ -138,6 +268,9 @@ export default {
           margin-bottom: 20px;
         }
       }
+      .bold {
+        font-weight: bold;
+      }
       .tema,.aluno {
         width: 100%;
       }
@@ -155,25 +288,103 @@ export default {
         width: 100%;
         li {
           font-size: 3rem;
+          margin-top: 50px;
         }
       }
-      .instalacao {
+      .eg-code-block {
+        border: 2px solid black;
+        border-radius: 10px;
+        box-shadow: 5px 5px 10px;
+        font-size: 2rem;
+      }
+      .instalacao, .classe, .teste-mocha, .teste-jest {
         margin-top: 50px;
         .eg-code-block {
           margin-bottom: 50px;
-          border: 2px solid black;
-          border-radius: 10px;
-          box-shadow: 5px 5px 10px;
-          font-size: 2rem;
         }
       }
-      .classe {
-        margin-top: 50px;
+      .teste-mocha, .teste-jest {
+        position: relative;
+        img {
+          position: absolute;
+          top: -125px;
+          right: -50px;
+          z-index: 2;
+        }
         .eg-code-block {
-          border: 2px solid black;
-          border-radius: 10px;
-          box-shadow: 5px 5px 10px;
+          font-size: 1.8rem;
+        }
+      }
+      .dirs {
+        width: 100%;
+        margin-top: 50px;
+        display: flex;
+        justify-content: space-around;
+        #jest, #mocha {
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+          p {
+            font-size: 2rem;
+          }
+        }
+      }
+      .sintaxe {
+        margin-top: 50px;
+        #jest {
+          margin-top: 100px;
+        }
+        #jest, #mocha {
+          position: relative;
+          img {
+            position: absolute;
+            top: -125px;
+            right: -50px;
+            z-index: 2;
+          }
+        }
+      }
+      .desempenho {
+        width: 100%;
+        font-size: 3rem;
+        margin-top: 50px;
+        display: flex;
+        justify-content: center;
+        table, th, td {
+          border: 1px solid black;
+          padding: 10px;
+        }
+        li {
+          margin-top: 50px;
+        }
+      }
+      .curva {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        margin: 0 auto;
+        margin-top: 50px;
+        p {
           font-size: 2rem;
+          margin-top: 10px;
+        }
+      }
+      .conclusao {
+        margin-top: 50px;
+        font-size: 1.75rem;
+        table, th, td {
+          border: 1px solid black;
+          padding: 10px;
+        }
+      }
+      .duvidas {
+        margin-top: 50px;
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        img {
+          width: 700px;
         }
       }
       #resultado {
